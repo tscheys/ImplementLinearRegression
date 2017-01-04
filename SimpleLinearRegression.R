faafe24cfa3bd6e549593cd439a00ebb64db14ef
tscheys <tim.scheys@gmail.com>
2017-01-04 14:53:05 +0100

    add catch block for unequal lengths of predictor and response

--- SimpleLinearRegression.R
+++ SimpleLinearRegression.R
@@ -3,27 +3,34 @@
 # x and y variables to test code 
 set.seed(100)
 x <- rnorm(100)
-y <- x + rnorm(100, mean=5, sd=2)
+y <- x + rnorm(102, mean=5, sd=2)
 
 getBetas = function(x, y) {
-  X = mean(predictor)
-  Y = mean(response)
-  numerator = 0
-  denumerator = 0
-  
-  for (i in 1:length(predictor)){
-    #calculating B1
-    #B1 = somm((xi - avg x)(yi-avg y)) / somm(xi - avg x)^2
-    numerator = numerator + (x[i] - X) * (y[i] - Y)
-    denumerator = denumerator + (x[i] - X)^2
+  #test if x and y are of equal length
+  if(length(x) != length(y)) {
+    tryCatch(simpleError("predictor and response are not of equal length!"))
+  } else {
+    X = mean(x)
+    Y = mean(y)
+    numerator = 0
+    denumerator = 0
+    
+    for (i in 1:length(x)){
+      #calculating B1
+      #B1 = somm((xi - avg x)(yi-avg y)) / somm(xi - avg x)^2
+      numerator = numerator + (x[i] - X) * (y[i] - Y)
+      denumerator = denumerator + (x[i] - X)^2
+    }
+    
+    B1 = numerator / denumerator
+    
+    B0 = Y - B1 * X
+    
+    return(c(B1,B0))
   }
-  
-  B1 = numerator / denumerator
-  
-  B0 = Y - B1 * X
-  
-  return(B1,B0)
 }
+
+test <- getBetas(x,y)
 
 #B1 = somm((xi - avg x)(yi-avg y)) / somm(xi - avg x)^2
 
