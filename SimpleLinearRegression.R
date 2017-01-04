ff04e7f84f6cae85b6130aad614b7e54b1b5e046
tscheys <tim.scheys@gmail.com>
2017-01-04 14:43:11 +0100

    implement beta calculation of SLR

--- SimpleLinearRegression.R
+++ SimpleLinearRegression.R
@@ -1,15 +1,28 @@
 # Implementation of Simple Linear Regression 
 
 # x and y variables to test code 
+set.seed(100)
 x <- rnorm(100)
 y <- x + rnorm(100, mean=5, sd=2)
 
-getBetas = function(predictor, response) {
+getBetas = function(x, y) {
   X = mean(predictor)
   Y = mean(response)
   numerator = 0
   denumerator = 0
   
+  for (i in 1:length(predictor)){
+    #calculating B1
+    #B1 = somm((xi - avg x)(yi-avg y)) / somm(xi - avg x)^2
+    numerator = numerator + (x[i] - X) * (y[i] - Y)
+    denumerator = denumerator + (x[i] - X)^2
+  }
+  
+  B1 = numerator / denumerator
+  
+  B0 = Y - B1 * X
+  
+  return(B1,B0)
 }
 
 #B1 = somm((xi - avg x)(yi-avg y)) / somm(xi - avg x)^2
