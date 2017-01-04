b4dd8a0eec524966279a3fc4c57cbde98043dfd2
tscheys <tim.scheys@gmail.com>
2017-01-04 16:15:24 +0100

    implement getFittedValues function

--- SimpleLinearRegression.R
+++ SimpleLinearRegression.R
@@ -3,7 +3,7 @@
 # x and y variables to test code 
 set.seed(100)
 x <- rnorm(100)
-y <- x + rnorm(102, mean=5, sd=2)
+y <- x + rnorm(100, mean=5, sd=2)
 
 getBetas = function(x, y) {
   #test if x and y are of equal length
@@ -21,25 +21,28 @@ getBetas = function(x, y) {
       numerator = numerator + (x[i] - X) * (y[i] - Y)
       denumerator = denumerator + (x[i] - X)^2
     }
-    
     B1 = numerator / denumerator
-    
+    #B0 = avg(y) - B1 * avg(x)
     B0 = Y - B1 * X
     
-    return(c(B1,B0))
+    return(c(B0,B1))
   }
 }
 
-test <- getBetas(x,y)
+getFittedValues <- function(coefficients, predictor) {
+  fittedValues = rep(0, length(predictor))
+  for(i in 1:length(predictor)) {
+    fittedValues = coefficients[1] + coefficients[2] * x
+  }
+  return(fittedValues)
+}
 
-#B1 = somm((xi - avg x)(yi-avg y)) / somm(xi - avg x)^2
-
-#B0 = avg(y) - B1 * avg(x)
-
-#formule met B0 en B1 invullen om parameters te bepalen 
+RSS = (y - getFittedValues(getBetas(x,y), x))^2
 
 #ook formule voor SE parameters maken
-
+#fitted.values = b0 + b1*x
+#RSS = (yi - y)^2 
+#RSE = sqrt(RSS/n-2) 
 #(SE B0) ^2 = o^2 (1/n +  [    (avg(x)^2)/somm(   (xi-avg(x))^2  ) ]    )
 
 
@@ -49,3 +52,5 @@ test <- getBetas(x,y)
 #BI: B +- 2SE(B)
 
 #formule om p waarde geassocieerd met Ho: B=0 te bepalen 
+
+# TESTS 
