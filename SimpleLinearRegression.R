a5a482a60b9036c9b07baa68dcdf8e6a24cbf587
tscheys <tim.scheys@gmail.com>
2017-01-04 14:37:59 +0100

    Comment formulas

--- SimpleLinearRegression.R
+++ SimpleLinearRegression.R
@@ -1,35 +1,31 @@
 # Implementation of Simple Linear Regression 
 
-implementLinearRegression
-
-vector met x waarden
-
 # x and y variables to test code 
 x <- rnorm(100)
 y <- x + rnorm(100, mean=5, sd=2)
 
-vector met y waarden 
+getBetas = function(predictor, response) {
+  X = mean(predictor)
+  Y = mean(response)
+  numerator = 0
+  denumerator = 0
+  
+}
 
-getParams(vector x, vector y)
+#B1 = somm((xi - avg x)(yi-avg y)) / somm(xi - avg x)^2
 
-avg(x)
+#B0 = avg(y) - B1 * avg(x)
 
-avg(y)
+#formule met B0 en B1 invullen om parameters te bepalen 
 
-B1 = somm((xi - avg x)(yi-avg y)) / somm(xi - avg x)^2
+#ook formule voor SE parameters maken
 
-B0 = avg(y) - B1 * avg(x)
-
-formule met B0 en B1 invullen om parameters te bepalen 
-
-ook formule voor SE parameters maken
-
-(SE B0) ^2 = o^2 (1/n +  [    (avg(x)^2)/somm(   (xi-avg(x))^2  ) ]    )
+#(SE B0) ^2 = o^2 (1/n +  [    (avg(x)^2)/somm(   (xi-avg(x))^2  ) ]    )
 
 
 
-ook formule voor betrouwbaarheidsintervallen maken (95%)
+#ook formule voor betrouwbaarheidsintervallen maken (95%)
 
-BI: B +- 2SE(B)
+#BI: B +- 2SE(B)
 
-formule om p waarde geassocieerd met Ho: B=0 te bepalen 
+#formule om p waarde geassocieerd met Ho: B=0 te bepalen 
