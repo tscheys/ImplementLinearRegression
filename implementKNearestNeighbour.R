58a65bea10073b61254fc5107ec915e02b6c2f8f
tscheys <tim.scheys@gmail.com>
2017-02-04 18:32:13 -0800

    implement KNN

--- implementKNearestNeighbour.R
+++ implementKNearestNeighbour.R
@@ -1,13 +1,47 @@
-x <- rnorm(100)
+# set up test data set 
+x1 <- rnorm(100)
+x2 <- rnorm(100)
 y <- as.factor(rep(c(0,1),times=50))
-x0 <- 0.4
+x <- cbind(x1,x2)
 
-
-KNN <- function(K, x, y, x0) {
-  distances = sqrt((x-x0)^2)
-  neighbours = sort.int(distances, decreasing=T, index.return = T)
-  df = data.frame(distances,y)
-  # order df and largest K values
-  df = df[with(df, order(-distances)),][1:K,]
-
+#calculate mode
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
 }
+
+KNN <- function(K, X, y) {
+  #predicts class labels with KNN method
+  #K = number of neighbours, constant
+  #X = predictors for y, matrix 
+  #y = class labels for observations, vector
+  
+  #prediction vector 
+  predictions = rep(0, length(y))
+  
+  #for all observations, select label that is most prevalant amongst its K nearest neighbours
+  for(i in 1:nrow(x)) {
+    #select one observation
+    obs <- x[i,]
+    #calculate euclidian distance between observation and all other observations
+    euclidians <- t(t(x) - obs)^2
+    #select K indexes with shortest euclidian distance 
+    indexes <- order(rowSums(euclidians))[1:K]
+    #get associated labels of K observations 
+    labels <- y[indexes]
+    #select most prevalant label as prediction
+    predictions[i] <- Mode(labels)
+  }
+  #give predictions 0,1 coding
+  return(predictions - 1)
+}
+
+#test function
+preds <- KNN(10,x,y)
+#check confusion matrix
+table(preds, y)
+
+#lessons learned: 
+#order is sort for vector indexes
+#cmd enter on function runs multiple lines 
+#to do: algorithm selects observation it needs to predict
